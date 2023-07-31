80ee90f89db7612f00464493b99afda823b6e684
MikeYuen <nhatminh.150596@gmail.com>
2023-07-31 17:20:04 +0700

    MM-31 handle invalid amount error smart contract

--- src/contracts/revertedMessages.ts
+++ src/contracts/revertedMessages.ts
@@ -2,6 +2,7 @@ export const ADDRESS_NOT_WHITELISTED_ERROR_MESSAGE = 'Address is not in allowlis
 export const USER_DENIED_TX_SIGNATURE = 'MetaMask Tx Signature: User denied transaction signature.'
 export const MAXIMUN_AMOUNT_REACHED = 'Exceed maximum allowlist mint amount for each address!'
 export const ADDRESS_ALREADY_CLAIMED = 'Address already claimed!'
+export const INVALID_MINT_AMOUNT = 'Invalid mint amount!'
 
 export const Messages = [
   {
@@ -10,5 +11,6 @@ export const Messages = [
   },
   { errorMessage: USER_DENIED_TX_SIGNATURE, userMessage: USER_DENIED_TX_SIGNATURE },
   { errorMessage: MAXIMUN_AMOUNT_REACHED, userMessage: MAXIMUN_AMOUNT_REACHED },
-  { errorMessage: ADDRESS_ALREADY_CLAIMED, userMessage: MAXIMUN_AMOUNT_REACHED }
+  { errorMessage: ADDRESS_ALREADY_CLAIMED, userMessage: MAXIMUN_AMOUNT_REACHED },
+  { errorMessage: INVALID_MINT_AMOUNT, userMessage: MAXIMUN_AMOUNT_REACHED }
 ]
